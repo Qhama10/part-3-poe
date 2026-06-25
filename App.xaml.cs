a66c4da42b0c2abe37abfd3970e0833da33138e3
Qhama10 <qhobongwana@gmail.com>
2026-06-25 16:50:59 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace part_3_poe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
